eda1244415323f5b43aebe5c0dc17f632af1637a
Sumit kumar <141808260+Sumitkumar0403@users.noreply.github.com>
2024-07-02 15:29:03 +0000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Sumit kumar",
+  title: "Web Developer & Content Creator",
+  email: "sumit16567@gmail.com",
+  gitHub: "Sumitkumar0403",
+  instagram: "sumit_0403_kumar",
+  linkedIn: "sumit-kumar-458563244",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "Sumit_0403",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
